57c119df5c0bb6b5477109b9c652d183ef8fb582
Tiago Fernandez <tiago.fernandez@gmail.com>
2015-02-18 09:43:10 +0100

    Code review + testing on both Chrome & Firefox

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 require('./main.css');
 
 var d = document,
-    _queryS = d.querySelector.bind(d);
+    _queryS = d.querySelector.bind(d),
+    _chatButtonId = 'naugtur-chat-button';
+
+if (_queryS('#' + _chatButtonId)) {
+    throw new Error("Bookmarklet already loaded.");
+}
 
 d.body.classList.add('naugtur');
 
@@ -12,18 +17,13 @@ setInterval(function () {
     }
 }, 2000);
 
-var _chatButtonId = 'naugtur-chat',
-    _chatButtonMissing = !_queryS('#' + _chatButtonId);
-
-if (_chatButtonMissing) {
-    var mybt = d.createElement('p');
-    mybt.setAttribute('id', _chatButtonId);
-    mybt.setAttribute('class', 'btn btn-large btn-l-gray btn-success');
-    mybt.setAttribute('title', 'https://naugtur.github.io/gs-is-for-chat/');
-    mybt.innerHTML = 'Chat';
-    mybt.addEventListener('click', function () {
-        d.body.classList.toggle('naugtur');
-        mybt.classList.toggle('btn-success');
-    });
-    _queryS('#queue-btns .btn-group').appendChild(mybt);
-}
+var mybt = d.createElement('p');
+mybt.setAttribute('id', _chatButtonId);
+mybt.setAttribute('class', 'btn btn-large btn-l-gray btn-success');
+mybt.setAttribute('title', 'https://naugtur.github.io/gs-is-for-chat/');
+mybt.innerHTML = 'Chat';
+mybt.addEventListener('click', function () {
+    d.body.classList.toggle('naugtur');
+    mybt.classList.toggle('btn-success');
+});
+_queryS('#queue-btns .btn-group').appendChild(mybt);
